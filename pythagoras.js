38ddb33960d858610f2fc68e01de1a8adeffb94a
raksha27yadav <52619596+raksha27yadav@users.noreply.github.com>
2020-10-02 14:20:19 +0530

    Update pythagoras.js

--- pythagoras.js
+++ pythagoras.js
@@ -1,1 +1,12 @@
+function calc(){
+
+  var base=document.getElementById('b').value;
+  var perp=document.getElementById('p').value;
+  var base=parseInt(base,10)
+  var perp=parseInt(perp,10)
+  var len=0;
+  len=(base*base)+(perp*perp)
+  len=Math.sqrt(len)
+  document.getElementById('res').innerHTML="length of third side is "+len
+}
 
